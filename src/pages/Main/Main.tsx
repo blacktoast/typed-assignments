c4315d9bf1788efb87ea6f9c7fb458f6e31eb9d0
blacktoast <dbstjd100@naver.com>
2023-03-25 22:08:01 +0900

    feat: add image add btn

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,15 +2,16 @@ import { TypedIcon } from 'typed-design-system';
 import Button from '../../components/Button/Button';
 import ResourceView from '../../components/ResourceView';
 import style from './Main.module.css';
-import { useEffect, useRef, useState } from 'react';
+import { ChangeEvent, DragEvent, useEffect, useRef, useState } from 'react';
 
 function Main() {
   const [isShowUrlInput, setShowUrlInput] = useState(false);
-  const [isShowImgInput, setShowImgInput] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDragging, setIsDragging] = useState(false);
+  const [image, setImage] = useState<File>();
 
   const inputUrl = useRef<HTMLInputElement>(null);
-  const inputImg = useRef<HTMLInputElement>(null);
+  const inputImgRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (isShowUrlInput && inputUrl.current) {
@@ -19,18 +20,14 @@ function Main() {
     }
   }, [isShowUrlInput]);
 
-  useEffect(() => {
-    if (isShowImgInput && inputImg.current) {
-      inputImg.current.focus();
-    }
-  }, [isShowImgInput]);
-
   const onClickUrlInput = () => {
     setShowUrlInput(true);
   };
 
   const onClickImgInput = () => {
-    setShowImgInput(true);
+    if (inputImgRef.current) {
+      inputImgRef.current.click();
+    }
   };
 
   const onFocusOutUrlInput = () => {
@@ -40,11 +37,12 @@ function Main() {
       setIsModalOpen(true);
     } else {
       setShowUrlInput(false);
+      console.log('store!');
       //나중에 저장 로직 구현
     }
   };
 
-  const handleReenter = () => {
+  const handleReEnter = () => {
     setIsModalOpen(false);
     setShowUrlInput(true);
     inputUrl.current?.focus();
@@ -55,9 +53,15 @@ function Main() {
     setShowUrlInput(false);
   };
 
-  const onFocusOutInput = () => {
-    setShowUrlInput(false);
-    setShowImgInput(false);
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const files = Array.from(event.target.files);
+      files.forEach((file) => {
+        // Process each uploaded image file here (e.g., display a preview, upload to a server, etc.)
+        console.log('Image file:', file);
+        setImage(file);
+      });
+    }
   };
 
   const validateUrl = (url: string) => {
@@ -65,13 +69,58 @@ function Main() {
     return urlPattern.test(url);
   };
 
+  const handleDragOver = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDrop = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(false);
+    const files = event.dataTransfer.files;
+    const hasFile = files && event.dataTransfer.files.length > 0;
+
+    if (hasFile) {
+      console.log(files);
+      const validFiles = Array.from(event.dataTransfer.files).filter(
+        (file) => file.type === 'image/png' || file.type === 'image/jpeg'
+      );
+
+      validFiles.forEach((file) => {
+        // Process each uploaded image file here (e.g., display a preview, upload to a server, etc.)
+        console.log('Image file:', file);
+      });
+    }
+  };
+
   return (
     <>
       <div className="flex">
-        <div className={style.sideBar}>
+        <div
+          className={style.sideBar}
+          style={{
+            border: isDragging ? '2px dashed gray' : 'none',
+          }}
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+        >
           <div className={style.sideBar__nav}>
             <Button title="URL 추가" onClick={onClickUrlInput}></Button>
             <Button title="이미지 추가" onClick={onClickImgInput}></Button>
+            <input
+              type="file"
+              ref={inputImgRef}
+              onChange={handleFileChange}
+              accept=".png, .jpg, .jpeg"
+              multiple
+              style={{ display: 'none' }}
+            />
+
             {isShowUrlInput && (
               <div className={style.inputContainer}>
                 <input
@@ -82,19 +131,10 @@ function Main() {
                 />
               </div>
             )}
-            {isShowImgInput && (
-              <div className={style.inputContainer}>
-                <input
-                  className={style.inputUrl}
-                  type="text"
-                  ref={inputImg}
-                  onBlur={onFocusOutInput}
-                />
-              </div>
-            )}
           </div>
         </div>
         <ResourceView />
+        {image && <img src={URL.createObjectURL(image)} alt="" />}
       </div>
 
       {isModalOpen && (
@@ -105,7 +145,7 @@ function Main() {
               URL 스킴에는 반드시 "http://" 또는 "https://" 이 입력 돼야 합니다.
             </p>
             <div className={style.modalActions}>
-              <Button title="다시 입력하기" onClick={handleReenter} />
+              <Button title="다시 입력하기" onClick={handleReEnter} />
               <Button
                 title="저장 하지 않고 종료"
                 onClick={handleExitWithoutSaving}
